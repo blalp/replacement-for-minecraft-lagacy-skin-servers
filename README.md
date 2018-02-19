cde504429b18e01677ca63870ddc127d46e09bb7
blalp <blalp@outlook.com>
2018-02-18 22:18:46 -0800

    Added the actual code and updated readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# replacement for minecraft lagacy skin servers
+I am hosting this at 159.69.202.67 and installation instructions for rerouting your computers attemps to acsess the outdated skin api to my servers are here https://www.superfuntime.org/forum/index.php/topic,183323.msg877218.html#msg877218 
+
+# If you want to host this yourself
+Download nodejs 8.9.4
+execute with node /path/to/main.js
+then follow installtion instructions https://www.superfuntime.org/forum/index.php/topic,183323.msg877218.html#msg877218 and change all instances of 159.69.202.67 to the IP address of your server
---
+++ main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var https = require('https');
+http.createServer(function(req, res) {
+        var path = require('path');
+        var uuid = req.url.toString().substr("/MinecraftSkins".length + 1).substring(0$
+        console.log(uuid);
+        var options = {host: 'api.mojang.com',port: 443,path: '/users/profiles/minecra$
+        console.log('api.mojang.com/users/profiles/minecraft/' + uuid);
+        var request = https.request(options, function(resy) {
+                var data = '';
+                resy.on('data', function(chunk) {
+                        data += chunk;
+                });
+                resy.on('end', function() {
+                        try {
+                                var json = JSON.parse(data);
+                                getImageFromUUID(json.id, res);
+                        } catch (err) {
+                                console.log(uuid +' is not a valid name');
+                                console.log(err + ' <> ' + data);
+                                res.end();
+                        }
+                });
+        });
+        request.on('error', function(e) {
+                console.log(e.message);
+        });
+        request.end();
+}).listen(8080);
+function getImageFromUUID(uuid, res) {
+        console.log('started uuid match with ' + uuid);
+        var options = {host: 'crafatar.com',port: 443,path: '/skins/' +uuid + '.png'};
+        var request1 = https.request(options,function(resyy) {
+                var data = '';
+                resyy.on('data', function(chunk) {
+                res.write(chunk);
+                });
+                resyy.on('end', function() {
+                        res.write(data);
+                        res.end();
+                });
+        });
+        request1.on('error', function(e) {
+                console.log(e.message);
+        });
+        request1.end();
+}
